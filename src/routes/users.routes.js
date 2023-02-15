847d72b31a85d951c0441195c7fff97fa5c67508
Jackson <jackson1712@hotmail.com>
2023-02-15 13:08:26 -0300

    routes added to separate functionality

---
+++ src/routes/users.routes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const usersRoutes = Router();
+
+const UsersController = require("../controllers/UsersController");
+const usersController = new UsersController();
+
+function myMiddleware(request, response, next) {
+    if(!request.body.isAdmin) {
+        return response.json({ message : "Não autorizado" })
+    }
+
+    next();
+}
+
+
+usersRoutes.post("/", myMiddleware, usersController.create);
+
+module.exports = usersRoutes;
