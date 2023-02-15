ee0ea34e082872bd776c10905ce788f13ff856a3
Jackson <jackson1712@hotmail.com>
2023-02-15 20:13:03 -0300

    added validation email in creation of user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,26 @@
 const AppError = require("../utils/AppError");
 const { hash, compare } = require("bcrypt");
+const knex = require("../database/knex");
 
 class UsersController {
-    create(request, response) {
-        const { id } = request.params;
+    async create(request, response) {
         const { email, name, password } = request.body;
 
-        return response.json({ email, name, password });
+        const checkUserExists = await knex("users").where({ email }).first();
+
+        if(checkUserExists) {
+            throw new AppError("Este email já está sendo usado", 401)
+        }
+
+    
+        await knex("users").insert({
+            name,
+            email,
+            password
+        })
+
+
+        return response.json();
     }
 }
 
