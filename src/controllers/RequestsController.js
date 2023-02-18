2e72e63778aafbd7e6bbf53ee3c2a8fe4da5440b
Jackson <jackson1712@hotmail.com>
2023-02-17 21:43:56 -0300

    refactored

---
+++ src/controllers/RequestsController.js
@@ -0,0 +1,10 @@
+const AppError = require("../utils/AppError");
+const knex = require("../database/knex");
+
+class RequestController {
+    async create() {
+        
+    }
+}
+
+module.exports = RequestController;
