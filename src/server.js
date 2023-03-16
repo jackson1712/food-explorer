7a45c7e04ced8cf953ece318e91d8e7929d13350
Jackson <jackson1712@hotmail.com>
2023-03-16 18:46:26 -0300

    refactored items

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const database = require("./database/sqlite");
 const routes = require("./routes");
 const uploadConfig = require("./configs/upload");
-
 const AppError = require("./utils/AppError");
+
+const cors = require("cors");
 
 database();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
@@ -32,6 +34,7 @@ app.use((error, request, response, next) => {
 });
 
 const PORT = 3333;
+
 app.listen(PORT, () => { 
     console.log(`Server is running on PORT: ${PORT}`)
 });
