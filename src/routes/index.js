6eac561a4fcd9d4803e2cf900e7643f95dc70dfb
Jackson <jackson1712@hotmail.com>
2023-03-30 18:22:33 -0300

    refactored to fix route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const routes = Router();
 
 const usersRoutes = require("./users.routes");
 const dishesRoutes = require("./dishes.routes");
+const favoritesRoutes = require("./favorites.routes");
 const requestsRoutes = require("./requests.routes");
 const categoryRoutes = require("./category.routes");
 const itemsRequestsRoutes = require("./items_requests.routes");
@@ -12,6 +13,7 @@ const sessionsRoutes = require("./sessions.routes");
 
 routes.use("/users", usersRoutes);
 routes.use("/dishes", dishesRoutes);
+routes.use("/favorites", favoritesRoutes);
 routes.use("/requests", requestsRoutes);
 routes.use("/category", categoryRoutes);
 routes.use("/items_requests", itemsRequestsRoutes);
