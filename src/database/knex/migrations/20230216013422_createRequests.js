d066fb873d166ab0e3068628c6d9c0846676290a
Jackson <jackson1712@hotmail.com>
2023-04-04 21:08:10 -0300

    refactored: refactored migrations to add column status

--- src/database/knex/migrations/20230216013422_createRequests.js
+++ src/database/knex/migrations/20230216013422_createRequests.js
@@ -3,6 +3,7 @@ exports.up = knex => knex.schema.createTable("requests", table => {
     table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
+    table.text("status").default("Pendente");
 });
 
 exports.down = knex => knex.schema.dropTable("requests");
